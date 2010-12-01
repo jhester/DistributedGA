52ebc1af95e20b1459e0b56fcb4ae9d47c04034e
phatness <jreid@clemson.edu>
2010-11-30 21:53:24 -0500

    Added option for no motion to directions (as direction 4)

--- src/constant.py
+++ src/constant.py
@@ -5,8 +5,8 @@ class constant_class:
 	mapheight = 40
 
         #a way to convert from direction to position
-        #0-up  1-right 2-down 3-left
-	directionconvert = [(0,-1), (1,0), (0,1), (-1,0)]
+        #0-up  1-right 2-down 3-left 4-no move
+	directionconvert = [(0,-1), (1,0), (0,1), (-1,0), (0,0)]
 
 
 
