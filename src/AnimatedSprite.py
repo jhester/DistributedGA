d3a621d63b13d671bd5accbb128449436efa13ae
jhester <josiah.hester@gmail.com>
2010-12-01 15:18:27 -0500

    Players are now animated when they move around the map! Looks freaking good too! Players will go around the map engaging each other, currently there is no support for death though, the sprites just sit there.

--- src/AnimatedSprite.py
+++ src/AnimatedSprite.py
@@ -21,9 +21,12 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.frames_per_direction = frames_per_direction
         self.direction = start_direction
         self.directionChanged = False
-        self.velocity = 3
+        self.velocity = 12
+        self.step = 0;
         self.tileX = tileX
+        self.lastTileX = tileX
         self.tileY = tileY
+        self.lastTileY = tileY
         self.moving = False
         self.rect = pygame.Rect(tileX*32, tileY*32, 32, 32)
         
@@ -36,13 +39,20 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.direction = direction
         self.directionChanged = True
         if direction == self.UP:
+            self.lastTileY = self.tileY
             self.tileY += -1
         if direction == self.DOWN:
+            self.lastTileY = self.tileY
             self.tileY += 1
         if direction == self.LEFT:
+            self.lastTileX = self.tileX
             self.tileX += -1
         if direction == self.RIGHT:
+            self.lastTileX = self.tileX
             self.tileX += 1
+        
+        # Reset the step
+        self.step = 0
     
     def gotoTile(self, tileX, tileY):
         # Figure out what general direction were going
@@ -58,8 +68,14 @@ class AnimatedSprite(pygame.sprite.Sprite):
         # Set the destination
         self.moving = True
         self.directionChanged = True
+        self.lastTileX = self.tileX
+        self.lastTileY = self.tileY
+
         self.tileX = tileX
         self.tileY = tileY;
+        
+        # Reset the step
+        self.step = 0
         
     def handleKeyUp(self, evt):
         if evt.key == pygame.K_a:
@@ -76,20 +92,13 @@ class AnimatedSprite(pygame.sprite.Sprite):
         # Note that this doesn't work if it's been more that self._delay
         # time between calls to update(); we only update the image once
         # then, but it really should be updated twice.
-        
-        
-        #Start moving to our destination, if we are moving. 
-        # If we have made it to our location stop
-      
-        # Were moving so update taking into account velocity and direction
-        
             
-        # Now animate
+        # Animate
         if self.directionChanged is True:
             self._frame = self.direction*self.frames_per_direction
             self.directionChanged = False
             
-        if t - self._last_update > self._delay:
+        if self.moving is True and t - self._last_update > self._delay:
             self._frame += 1
             if self._frame >= len(self._images) / 4 + self.direction*self.frames_per_direction: self._frame = self.direction*self.frames_per_direction
             self.image = self._images[self._frame]
@@ -101,9 +110,28 @@ class AnimatedSprite(pygame.sprite.Sprite):
         startYTile = math.floor(float(yvpCoordinate) / tileHeight)
         xdiff = xvpCoordinate-startXTile*tileHeight
         ydiff = yvpCoordinate-startYTile*tileHeight
-
+        xdstep=0
+        ydstep=0
+        
+        start = (screenOffset[0]-xdiff+(self.lastTileX-startXTile)*32,screenOffset[1]-ydiff+(self.lastTileY-startYTile)*32)
+        end = (screenOffset[0]-xdiff+(self.tileX-startXTile)*32,screenOffset[1]-ydiff+(self.tileY-startYTile)*32)
+            
+        # Calculate where we moving to and where we coming from
+        #if self.lastTileX != self.tileX or self.lastTileY != self.tileY: 
+        if self.step != self.velocity:
+            self.step+=1
+            xdstep = (end[0]-start[0]) / self.velocity
+            ydstep = (end[1]-start[1]) / self.velocity
+            start = (start[0]+xdstep*self.step,start[1]+ydstep*self.step)
+        else:
+            self.step=0  
+            self.moving = False
+            self.lastTileX = self.tileX
+            self.lastTileY = self.tileY
+            start = end
+           
         #screen.blit(self.image, (screenOffset[0]+xvpCoordinate - (startXTile * tileHeight), screenOffset[1]+yvpCoordinate - (startYTile * tileHeight)))
         #screen.blit(self.image, screenOffset, (xvpCoordinate - (startXTile * tileHeight), yvpCoordinate - (startYTile * tileHeight)) + vpDimensions)
-        screen.blit(self.image, (screenOffset[0]-xdiff+(self.tileX-startXTile)*32,screenOffset[1]-ydiff+(self.tileY-startYTile)*32))
+        screen.blit(self.image, start)
         #screen.blit(self.image, self.rect.topleft)
         
