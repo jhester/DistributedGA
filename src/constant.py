b2b9b91a69fee0032c6ec6fc9456ce9f88e998af
phatness <jreid@clemson.edu>
2010-12-02 10:05:03 -0500

    Added packet codes to constant.

--- src/constant.py
+++ src/constant.py
@@ -1,6 +1,10 @@
 class constant_class:
+	#client codes
 	observercode = 1
 	clientcode = 0
+
+	#I don't think this is used
+	#please check, and delete if not used
 	mapwidth = 40 
 	mapheight = 40
 
@@ -8,5 +12,7 @@ class constant_class:
         #0-up  1-right 2-down 3-left 4-no move
 	directionconvert = [(0,-1), (1,0), (0,1), (-1,0), (0,0)]
 
-
-
+	#packet codes
+	packet_heartbeat = 0
+	packet_spawn = 1
+	packet_main = 2
